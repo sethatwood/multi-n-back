76030c7cf35352cb617486b1cf62eca9c0f986b5
Seth Atwood <sethmerrick@gmail.com>
2023-11-11 14:37:13 -0500

    feat: adds `Stimulus`

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,19 @@
 <template>
-  <div class="app">
-    <h1>Poly N-Back Game</h1>
-    <!-- Your game components will go here -->
+  <div class="container mx-auto px-4 py-5">
+    <h1 class="text-xl font-bold text-center mb-5">Poly N-Back Game</h1>
+    <Stimulus position="left" color="purple" shape="circle" />
+    <!-- Additional game components will go here -->
   </div>
 </template>
 
 <script>
+import Stimulus from './components/Stimulus.vue';
+
 export default {
   name: 'App',
+  components: {
+    Stimulus,
+  },
   // Any component-specific logic goes here
 };
 </script>
-
-<style>
-/* Add some basic styling */
-.app {
-  text-align: center;
-  margin-top: 50px;
-}
-</style>
