c2b2639138e825069124bd82cca2ca580ba48220
Seth Atwood <sethmerrick@gmail.com>
2023-11-13 00:55:21 -0500

    feat: improves intro modal

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,41 @@
 <template>
   <div v-if="showModal" class="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto" id="howToPlayModal">
     <div class="relative top-20 mx-auto p-5 border container shadow-lg rounded-md bg-white">
-        <div class="instructions mt-2 px-4">
-          <h2 class="text-xl font-semibold">How to Play</h2>
-          <p class="text-md mt-2">
-            • Click the buttons (Position, Color, Shape) that match
-            the stimuli you saw <strong>{{ gameStore.nBack }}</strong> turns ago.
-            Aim to remember and match each attribute (position, color, shape) as it appears.
-          </p>
-          <p class="text-md mt-2">
-            • Correct responses increase your score. Incorrect responses decrease it
-            and count as a strike.
-          </p>
-          <p class="text-md mt-2">
-            • The game ends if you accumulate 3 strikes. Aim to achieve the highest score possible
-            without hitting 3 strikes.
-          </p>
-          <p class="text-md mt-2">
-            • Your high score is recorded, so you can see your progress over time.
-          </p>
-        </div>
-        <div class="items-center px-4 py-3">
-          <button @click="startGame" class="px-4 py-2 bg-blue-500 text-white text-base font-medium rounded-md w-full shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-300">
-            Start Game
-          </button>
-        </div>
+      <div class="instructions mt-2 px-4">
+        <h2 class="text-xl font-semibold">Welcome to Poly N-Back!</h2>
+        <p class="text-md mt-2">
+          Poly N-Back is a variation of the <a href="https://en.wikipedia.org/wiki/N-back" class="text-blue-500 hover:text-blue-700 underline" target="_blank">N-back</a> memory game designed to improve your cognitive skills</p>
+        <p class="text-md mt-2 font-semibold">
+          Here's how you can play:
+        </p>
+        <p class="text-md mt-2">
+          • Click the buttons (Position, Color, Shape) that match the stimuli you saw <strong>{{ gameStore.nBack }}</strong> turns ago. Aim to remember and match each attribute (position, color, shape) as it appears.
+        </p>
+        <p class="text-md mt-2">
+          • Correct responses increase your score. Incorrect responses decrease it and count as a strike.
+        </p>
+        <p class="text-md mt-2">
+          • The game ends if you accumulate 3 strikes. Aim to achieve the highest score possible without hitting 3 strikes.
+        </p>
+        <p class="text-md mt-2">
+          • Your high score is recorded, so you can see your progress over time.
+        </p>
+      </div>
+      <div class="items-center px-4 py-3">
+        <button @click="startGame" class="px-4 py-2 bg-blue-500 text-white text-base font-medium rounded-md w-full shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-300">
+          Start Game
+        </button>
+      </div>
+      <div class="text-center text-sm py-2">
+        Built by Human+AI collaboration with 💜
+      </div>
     </div>
   </div>
   <div class="container mx-auto px-4 py-5 text-center text-white bg-slate-700">
     <h1 class="text-2xl font-bold mb-5">Poly N-Back</h1>
     <div class="mb-5">
-      <p>Time Left: {{ gameStore.timeLeft }}s</p>
-     <p class="text-md text-red-500">Strikes: {{ gameStore.incorrectResponses }}</p>
+      <p class="text-sm uppercase text-red-500">Strikes: {{ gameStore.incorrectResponses }}</p>
+      <p class="text-xl font-bold">{{ gameStore.timeLeft }}</p>
     </div>
     <Stimulus
       class="mb-5"
@@ -62,7 +66,7 @@
     </div>
     <div class="mt-5 text-center">
       <p :class="scoreClass">Score: {{ gameStore.score }}</p>
-      <p class="text-sm text-green-500">High Score: {{ gameStore.highScore }}</p>
+      <p class="text-sm uppercase text-green-500">High Score: {{ gameStore.highScore }}</p>
       <button v-if="gameStore.incorrectResponses >= 3"
               @click="startGame"
               class="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
