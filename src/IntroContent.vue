71d0e2e8e2b889f0255415bd75f8175a93b0ca48
Seth Atwood <sethmerrick@gmail.com>
2023-12-13 12:08:22 -0500

    feat: responsive intro text

--- src/IntroContent.vue
+++ src/IntroContent.vue
@@ -1,19 +1,19 @@
 <template>
-  <div class="instructions mt-2 px-4">
+  <div class="instructions text-xs md:text-md lg:text-lg mt-2 px-4">
     <p class="text-md mt-4 font-semibold">
       Here's how you can play:
     </p>
-    <p class="text-md mt-2">
-      • Click the buttons that match the attributes you saw <strong>{{ gameStore.nBack }}</strong> turns ago. (Color, Emoji, Position, Shape)
+    <p class="mt-2">
+      • Tap attributes from <strong>{{ gameStore.nBack }}</strong> turns ago.<br>&nbsp;&nbsp;[Color, Emoji, Position, Shape]
     </p>
-    <p class="text-md mt-2">
+    <p class="mt-2">
       • Correct responses increase your score.
     </p>
-    <p class="text-md mt-2">
-      • The game ends if you accumulate 3 strikes.
+    <p class="mt-2">
+      • The game ends after 3 strikes.
     </p>
-    <p class="text-md mt-2">
-      • Your high score is recorded, so you can see your progress over time.
+    <p class="mt-2">
+      • High scores are saved.
     </p>
   </div>
 </template>
